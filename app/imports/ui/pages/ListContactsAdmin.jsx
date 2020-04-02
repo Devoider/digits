a63924777ba3faf01024ec3581d7c40c81167082
Devoider <Drakon3001@gmail.com>
2020-04-01 20:07:58 -1000

    contacts collection finished in 35 minutes

--- app/imports/ui/pages/ListContactsAdmin.jsx
+++ app/imports/ui/pages/ListContactsAdmin.jsx
@@ -3,7 +3,7 @@ import { Meteor } from 'meteor/meteor';
 import { Container, Header, Loader, Card } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
-import Contact from '/imports/ui/components/Contact';
+import ContactsAdmin from '/imports/ui/components/Contact';
 import { Contacts } from '../../api/contact/Contacts';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
@@ -18,9 +18,9 @@ class ListContacts extends React.Component {
   renderPage() {
     return (
         <Container>
-          <Header inverted as="h2" textAlign="center">List Contacts</Header>
+          <Header inverted as="h2" textAlign="center">List Contacts (Admin)</Header>
           <Card.Group>
-            {this.props.contacts.map((contact, index) => <Contact key={index} contact={contact}/>)}
+            {this.props.contacts.map((contact, index) => <ContactsAdmin key={index} contact={contact}/>)}
           </Card.Group>
         </Container>
     );
@@ -36,7 +36,7 @@ ListContacts.propTypes = {
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(() => {
   // Get access to Stuff documents.
-  const subscription = Meteor.subscribe('Contacts');
+  const subscription = Meteor.subscribe('ContactsAdmin');
   return {
     contacts: Contacts.find({}).fetch(),
     ready: subscription.ready(),
