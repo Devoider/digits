3328c013b006bd13e51985ecdd4a726d8fa615c8
Devoider <Drakon3001@gmail.com>
2020-04-01 19:29:31 -1000

    Contacts Collection finished in 90 min

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,16 +1,15 @@
 import { Meteor } from 'meteor/meteor';
-import { Stuffs } from '../../api/stuff/Stuff.js';
-
+import { Contacts } from '../../api/contact/Contacts.js';
 /* eslint-disable no-console */
 
 /** Initialize the database with a default data document. */
 function addData(data) {
-  console.log(`  Adding: ${data.name} (${data.owner})`);
-  Stuffs.insert(data);
+  console.log(`  Adding: ${data.lastName} (${data.owner})`);
+  Contacts.insert(data);
 }
 
 /** Initialize the collection if empty. */
-if (Stuffs.find().count() === 0) {
+if (Contacts.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.map(data => addData(data));
